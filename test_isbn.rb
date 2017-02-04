4512a1ed5d9337bf9224a677aee7c5d0335811bc
Spac3Man2 <Spac3Man2>
2017-02-03 20:01:05 -0500

    isbn_test

--- test_isbn.rb
+++ test_isbn.rb
@@ -7,4 +7,8 @@ class Test_isbn <Minitest::Test
   def test_isbn_0471958697
 	assert_equal(7, check_sum("0471958697"))
   end	
+
+  def test_isbn_0321146530
+    assert_equal("0321146530", remove_spaces_dashes("0-321-14653-0"))
+  end
 end
