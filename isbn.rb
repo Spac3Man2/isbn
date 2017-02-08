ca907fb203b99e41346df5d1d68d68ff72b25c60
Spac3Man2 <Spac3Man2>
2017-02-08 16:16:03 -0500

    test_isbn

--- isbn.rb
+++ isbn.rb
@@ -9,16 +9,34 @@ def remove_spaces_dashes(num)
 		   formatted_number += letter
 
         # if letter == "x" || letter =="X"
-
-
-
 	    end
-
+	    
 	end
-
 	formatted_number
+end
+def check_for_symbols(symbol_check)
+    symbol_check2 = symbol_check.split("")
+    refreshed = []	
+    symbol_check2.each do |filter|
+     
+     if filter == "0" || filter == "1" ||
+        filter == "2" || filter == "3" ||
+    	filter == "4" || filter == "5" ||
+    	filter == "6" || filter == "7" ||
+    	filter == "8" || filter == "9" 
+    	#filter == "x" || filter == "X"
+	    refreshed.push(filter) 
+        end   
+    end
+    refreshed.length == symbol_check2.length
+end	
 
-end			
+def check_x(letter_x)
+	if letter_x.upcase.include?("X")
+		puts letter_x.upcase.index("X") == letter_x.length - 1
+    end
+    true
+end
 
 def check_ISBN10?(isbn) 
 
@@ -29,8 +47,6 @@ def check_ISBN10?(isbn)
 print sum
 return sum
 end  
-
-
 
 def check_sum(isbn)
 
@@ -48,8 +64,8 @@ def check_sum(isbn)
 
   end
 
- sum %11
-
+ (sum %11).to_s
+ 
 end
 
 def calculate_checksum(num)
@@ -73,7 +89,28 @@ def calculate_checksum(num)
 			index += 1
 		end
 
-		return ((10 - (sum % 10)) % 10)
+		return ((10 - (sum % 10)) % 10).to_s
 
     end
 end    
+
+def valid_isbn?(number_string)
+	no_spaces_no_dashes = remove_spaces_dashes(number_string)
+    
+    if no_spaces_no_dashes.length == 10 && check_for_symbols(no_spaces_no_dashes) && check_x(no_spaces_no_dashes)
+              print no_spaces_no_dashes[-1]
+
+       if no_spaces_no_dashes[-1].upcase == "X" 
+       return check_sum(no_spaces_no_dashes) == "10"	
+      
+       end
+       check_sum(no_spaces_no_dashes) == no_spaces_no_dashes[-1]
+
+    elsif
+       no_spaces_no_dashes.length == 13 && check_for_symbols(no_spaces_no_dashes)
+       calculate_checksum(no_spaces_no_dashes) == no_spaces_no_dashes[-1]
+    else 
+       return false
+    end   
+  
+end	
--- test_isbn.rb
+++ test_isbn.rb
@@ -5,7 +5,7 @@ require_relative "isbn.rb"
 class Test_isbn <Minitest::Test
 
   def test_isbn_0471958697
-	assert_equal(7, check_sum("0471958697"))
+	assert_equal("7", check_sum("0471958697"))
   end	
 
   def test_isbn_0321146530
@@ -13,7 +13,7 @@ class Test_isbn <Minitest::Test
   end
 
   def test_isbn_877195869x
-  	assert_equal(10, check_sum("877195869x"))
+  	assert_equal("10", check_sum("877195869X"))
   end
   
   def test_if_11_numbers_return_false
@@ -21,8 +21,44 @@ class Test_isbn <Minitest::Test
   end
 
   def test_13_numbers_9780470059029
-    assert_equal(9, calculate_checksum("9780470059029"))
+    assert_equal("9", calculate_checksum("9780470059029"))
   end
 
-  def  
+def test_random_stuff
+	assert_equal(false, check_for_symbols("&^86793"))
+    end
+
+def test_if_letter_returns_true
+	assert_equal(true, check_x("98763X"))
+    end
+
+def test_if_letter_X_is_still_true
+    assert_equal(false, check_x("563X977"))    
+    end
+def test_if_isbn_13_and_10_pass_together
+    assert_equal(true, valid_isbn?("978-0-13-149505-0"))  
+    end
+
+def test_invalid_13
+    assert_equal(false, valid_isbn?("978-0-13-6729-56-0"))  
+    end
+
+def test_invalid_13_with_X
+    assert_equal(false, valid_isbn?("978-0-13-6729-56-X"))  
+    end 
+
+def test_if_isbn_13_and_10_pass
+    assert_equal(false, valid_isbn?("978-$-13-149505-0"))  
+    end
+
+def test_valid_10_digit_isbn
+    assert_equal(true, valid_isbn?("0321146530")) 
+    end  
+
+def test_valid_10_digit_isbn767
+    assert_equal(true, valid_isbn?("0-321 14 6530")) 
+    end  
+def test_valid_10_digit_isbn_x
+    assert_equal(true, valid_isbn?("877195869x")) 
+    end                   
 end
